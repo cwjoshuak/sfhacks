42ad9f5ecac6b01707103734564d93352825f900
AthisaV <tincank@gmail.com>
2018-03-17 14:44:21 -0700

    Alpha #1

---
+++ js/init.js
@@ -0,0 +1,29 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+    $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+    });
+
+    $('input.autocomplete').autocomplete({
+    data: {
+      "Apple": null,
+      "Microsoft": null,
+    },
+    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+    onAutocomplete: function(val) {
+      // Callback function when value is autcompleted.
+    },
+    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+    });
+    
+  }); // end of document ready
+
+})(jQuery); // end of jQuery name space
